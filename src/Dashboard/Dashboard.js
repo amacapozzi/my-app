ded2cd82242686d2da0e0d5609d147a8c0289eda
ama.capozzi <amadeocapozzi07@gmail.com>
2022-06-15 22:11:56 +0100

    Dashboard fix

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,10 +1,19 @@
 import React  from "react";
+import  ButtonSend from '../components/ButtonSend';
+
 
 
 function Dashboard () {
     return (
         <section id="dashboard">
-
+            
+            <center>
+            <input placeholder="Ingrese usuario de ig"></input>
+            <br/>
+            
+            <ButtonSend/>
+            
+            </center>
         </section>
     )
 }
---
+++ src/components/ButtonSend.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+function ButtonSend () {
+    return(
+        
+        <button  class="btn__emp">Enviar Folowers <i class='bx bx-arrow-back bx-rotate-180' ></i></button>
+       
+    )
+}
+export default ButtonSend;
+
